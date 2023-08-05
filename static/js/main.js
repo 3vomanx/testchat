edbd6d2b0f46810017a97e429f87f72912395d7d
3vomanx <giorgigogidze39@gmail.com>
2023-08-05 16:35:43 +0400

    .

--- static/js/main.js
+++ static/js/main.js
@@ -78,7 +78,7 @@ socket.on('chat message partner', function (msg) {
     $(newData).appendTo($('#messages')).slideDown(speed=200,callback = function(){
         $("#messages").scrollTop($("#messages")[0].scrollHeight);
     });
-    $('#messages .partner').css('background', "#292929");
+    $('#messages .partner').css('background', "#FFA74D");
 
 });
 
